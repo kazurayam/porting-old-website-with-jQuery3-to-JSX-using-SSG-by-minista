f6732ac8f8b5db6c826eb4961399ce495cf717e8
kazurayam <kazuaki.urayama@gmail.com>
2026-09-11 20:46:29 +0900

    #1 got a success

--- my-minista-project/src/assets/js/windowResize.js
+++ my-minista-project/src/assets/js/windowResize.js
@@ -1,9 +1,26 @@
 // js/windowResize.js
-$(function () {
-    $(window).on('resize', function () {
-        const w = $(window).width()
-        const h = $(window).height()
-        const dimension = w + ' x ' + h
-        $('.myheader h1').text(dimension)
-    }).trigger('resize');
+import { $ } from '/src/assets/js/jquery-4.0.0.module.min'
+
+function waitForJquery(callback) {
+    const intervalId = setInterval(() => {
+        if (typeof($) !== 'undefined') {
+            clearInterval(intervalId);
+            console.log('cleared the interval');
+            callback();
+        } else {
+            console.log('waiting for $ to be defined')
+        }
+    }, 100); // Check every 1000ms
+}
+
+waitForJquery(() => {
+    $(function () {
+        $(window).on('resize', function () {
+            const w = $(window).width()
+            const h = $(window).height()
+            const dimension = w + ' x ' + h
+            console.log(dimension)
+            $('.myheader h1').text(dimension)
+        }).trigger('resize');
+    });
 });
